1099c5c1bce1cb3943aa992b94aad3a8b9d95c90
Adam Gray <adamgray@Adams-MacBook-Pro.local>
2018-08-06 17:24:47 +0100

    Initial commit

---
+++ src/com/adamgray/todolist/datamodel/TodoItem.java
@@ -0,0 +1,43 @@
+package com.adamgray.todolist.datamodel;
+
+import java.time.LocalDate;
+
+/**
+ * Created by adamgray on 05/04/2018.
+ */
+public class TodoItem {
+    private String shortDescription;
+    private String details;
+    private LocalDate deadline;
+
+    public TodoItem(String shortDescription, String details, LocalDate deadline) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+}
